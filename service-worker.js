3cdbc8d5713fe591ffbac7a7f024a6320cff2b09
transitdork <mattpgardiner@gmail.com>
2025-08-31 17:22:46 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 self.addEventListener('install', event => \{\
+  event.waitUntil(\
+    caches.open('drill-cache-v1').then(cache => \{\
+      return cache.addAll([\
+        'basketball-drill.html',\
+        'manifest.json'\
+      ]);\
+    \})\
+  );\
+\});\
+\
+self.addEventListener('fetch', event => \{\
+  event.respondWith(\
+    caches.match(event.request).then(response => \{\
+      return response || fetch(event.request);\
+    \})\
+  );\
+\});\
+}
